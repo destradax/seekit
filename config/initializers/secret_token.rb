8d2d839d586dc0d1486b8ff5e80461da49068458
Daniel Estrada <destradax@gmail.com>
2013-10-29 22:20:05 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Seekit::Application.config.secret_key_base = '03c08653b3f0cbb034402bde277cbd5db0ce0fff7ad24c269d0a33a50458f7c9229a61c304ba9731b9e555ef5b86e891a8feb2551f1c6af667c0054f57eef645'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Seekit::Application.config.session_store :cookie_store, key: '_seekit_session'
