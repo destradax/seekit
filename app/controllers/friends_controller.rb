bd5068ede4e8c1df67c83338b6f95a848076ce92
detradax <destradax@gmail.com>
2013-11-07 23:29:46 -0500

    /friends/get now returns the users (whithout their password)

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -35,10 +35,12 @@ class FriendsController < ApplicationController
 	# 	* friends: Array of User - the friends of the user
 	def get
 		if request.post?
-			# TODO get the actual users, not only their id's
-			# TODO don't return the passwords of the users
-			friends = Friendship.where(user_id: params[:user_id])
-			render json: friends
+			user = User.find_by_id(params[:user_id])
+			unless user
+				render json: {error: "user not fonud"}
+			end
+			
+			render json: user.friends.each {|f| f.password = nil}
 		end
 	end
 
