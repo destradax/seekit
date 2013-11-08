fb4b31da21162634b6b713e82d0dc0910eb9e860
detradax <destradax@gmail.com>
2013-11-07 22:30:03 -0500

    Created quests controller with two actions and updated doc

---
+++ app/controllers/quests_controller.rb
@@ -0,0 +1,47 @@
+class QuestsController < ApplicationController
+	# Gets the quests completed by the user
+	# [Input]
+	# 	* user_id: integer - the id of the current user
+	# [Output]
+	# 	* quests: Array of Quest - the quests completed by the user
+	# 	* error: string - in case the user is not found
+	def get_completed
+		user = User.find_by_id(params[:user_id])
+		unless user
+			render json: {error: "user not found"} and return
+		end
+		render json: user.quests
+	end
+
+	# Tries to complete a quest
+	# [Input]
+	# 	* user_id: integer - the id of the current user
+	# 	* quest_id: integer - the id of the quest the user is trying to compelte
+	# [Output]
+	# 	* msg: string - in case the quest is marked as completed
+	# 	* error: string - in case the quest can't be marked as completed
+	def seekit
+		user = User.find_by_id(params[:user_id])
+		unless user
+			render json: {error: "user not found"} and return
+		end
+		quest = Quest.find_by_id(params[:quest_id])
+		unless quest
+			render json: {error: "quest not found"} and return
+		end
+		if CompletedQuest.exists?(user_id: user.id, quest_id: quest.id)
+			render json: {error: "user already completed this quest"}
+		end
+		
+		# TODO check for the distance between the user and the quest
+
+		cq = CompletedQuest.new
+		cq.user = user
+		cq.quest = quest
+		if cq.save
+			render json: {msg: "quest completed"}
+		else
+			render json: {error: "could not complete quest"}
+		end
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Seekit::Application.routes.draw do
 	post "friends/add"
 	post "friends/get"
 	post "friends/remove"
+	post "quests/get_completed"
+	post "quests/seekit"
   
 	# The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
