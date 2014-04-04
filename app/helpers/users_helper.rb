2a769736b747a7ac120d0277449c67e1683220dc
destradax <destradax@gmail.com>
2014-04-03 23:34:27 -0500

    Made user show view pretty

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+ 
+ def user_profile_pic(user)
+  if @user.imageURL=="" || @user.imageURL.nil?
+   image_tag "default_pic.png", class: "img-thumbnail"
+  else
+   image_tag @user.imageURL, class: "img-thumbnail"
+  end
+ end
+ 
 end
