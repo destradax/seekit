99868dad1fb3d61780f8f0e978937960b3a2c267
destradax <destradax@gmail.com>
2014-04-05 14:35:56 -0500

    Fixed quest show view

--- app/helpers/quests_helper.rb
+++ app/helpers/quests_helper.rb
@@ -1,2 +1,18 @@
 module QuestsHelper
+
+	def quest_pics(quest)
+		pics = ""
+
+		if @quest.images.any?
+			@quest.images.each do |image|
+				if image.url
+					pics += image_tag image.url, class: "img-thumbnail"
+				end
+			end
+		else
+			pics += image_tag "default_pic.png", class: "img-thumbnail img-responsive"
+		end
+
+		pics.html_safe
+	end
 end
