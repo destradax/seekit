353abab4ae80227fe7f9abae074fef6276de7b55
destradax <destradax@gmail.com>
2014-04-08 22:47:50 -0500

    Added google map to select coordinates

---
+++ app/assets/javascripts/quests.js
@@ -0,0 +1,23 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://coffeescript.org/
+function initialize() {
+	var mapOptions = {
+		center: new google.maps.LatLng(6.23623163652871, -75.58052300009876),
+		zoom: 17
+	};
+	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+	google.maps.event.addListener(map, "click", function(event) {
+		var lat = event.latLng.lat();
+		var lng = event.latLng.lng();
+		// populate yor box/field with lat, lng
+		$("#quest_latitude").val(lat);
+		$("#quest_longitude").val(lng);
+	});
+}
+
+
+$(document).ready(function() {
+	initialize();
+});
