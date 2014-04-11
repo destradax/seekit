9592c03381bae441373b6768bbd9ac36bb6b57c5
destradax <destradax@gmail.com>
2014-04-10 21:56:44 -0500

    Removed Google Maps API key

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,2 @@
 module ApplicationHelper
-	def google_maps_api_url
-		api_key = "AIzaSyCYXyVuRA57ak78Xc9mncpylfFA1J9gVYM"
-		"https://maps.googleapis.com/maps/api/js?key=#{api_key}&sensor=false"
-	end
 end
